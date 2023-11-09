ce702d746f31c947833d711b3f0c81e93b40fbe2
kukuwwka <ququwka1@gmail.com>
2023-11-10 00:08:55 +0400

    initial commit

--- src/main/java/SomeGame/Main.java
+++ src/main/java/SomeGame/Main.java
@@ -1,20 +1,23 @@
 package SomeGame;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>();
-        Integer[] count = {1, 2, 3, 4, 8, 1, 9, 0, 5, 3};
-        IntStream.range(0, 9)
-    for (int i = 0; i < count.length; i++) {
-            for (int j = 0; j < count.length; j++) {
-                if (j != i && (count[i] + count[j]) == sum) {
-                    System.out.println();
-                }
-            }
-        }
+        int[] count = {1, 2, 3, 4, 8, 1, 9, 0, 5, 3};
+        int value = 9;
+        int foundItem = Arrays.binarySearch(count, value);
     }
 }
+
+/* Алгоритм:
+1. Задаём массив;
+2. Задаём переменную 9 - сумма двух стоящих рядом значений индексов в массиве;
+3. Метод, ищущий два одинаковых числа, стоящих рядом;
+4. Метод, вычёркивающий два значения массива из п.3 и п.4, а так же счётик к нему;
+5. Цикл, заканчивающий игру, если не осталось похожих значений из п.3 и п.4.
+6. Мои полномочия всё
+ */
--- src/main/java/Ternary/Ternary.java
+++ src/main/java/Ternary/Ternary.java
@@ -1,12 +1,6 @@
 package Ternary;
 
 public class Ternary {
-//    public static void main(String[] args) {
-//        int number = 123;
-//        String convertedNumber = Ternary.convertNumber(number);
-//        System.out.println(convertedNumber);
-//    }
-// проверка
     public static String convertNumber(int number) {
         String result;
         result = (number > 0) ? (number % 7 < 4) ? ("positive-m") : ("positive-p") : (number == 0) ? ("zero") : (number % 7 > -4) ? ("negative-m") : ("negative-p");
