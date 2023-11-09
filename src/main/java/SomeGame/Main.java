caefe475e4e3eaeeb1b1e46558abd97cc7f2cccb
kukuwwka <ququwka1@gmail.com>
2023-11-09 22:35:51 +0300

    initial commit

--- src/main/java/SomeGame/Main.java
+++ src/main/java/SomeGame/Main.java
@@ -1,13 +1,15 @@
 package SomeGame;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
-        int[] count = {1, 2, 3, 4, 8, 1, 9, 0, 5, 3};
-        int sum = 9;
-        for (int i = 0; i < count.length; i++) {
+        Integer[] count = {1, 2, 3, 4, 8, 1, 9, 0, 5, 3};
+        IntStream.range(0, 9)
+    for (int i = 0; i < count.length; i++) {
             for (int j = 0; j < count.length; j++) {
                 if (j != i && (count[i] + count[j]) == sum) {
                     System.out.println();
